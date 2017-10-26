2ad22d6b28a33f8417ce5d22e61bbcbc6e5f059e
Michael Dekker <git@michaeldekker.nl>
2017-10-26 03:32:59 +0200

    Signal that the carrier has been processed

--- views/js/fo_aeuc_tnc.js
+++ views/js/fo_aeuc_tnc.js
@@ -22,44 +22,49 @@
  * PrestaShop is an internationally registered trademark & property of PrestaShop SA
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
+  aeuc_controller = new AEUC_Controller();
 
-    aeuc_controller = new AEUC_Controller();
-
-    if (!!$.prototype.fancybox) {
-        $("a.iframe").fancybox({
-            'type': 'iframe',
-            'width': 600,
-            'height': 600
-        });
-    }
-
-    $('button[name="processCarrier"]').click(function(event){
-        /* Avoid any further action */
-        event.preventDefault();
-        event.stopPropagation();
-
-        if (aeuc_has_virtual_products === true && aeuc_controller.checkVirtualProductRevocation() === false)
-        {
-            var to_display = $('<div/>').html(aeuc_virt_prod_err_str).text();
-            $.fancybox(to_display,{
-                minWidth: 'auto',
-                minHeight: 'auto'
-            });
-            return;
-        }
-        $("#form").submit();
+  if ($.prototype.fancybox) {
+    $('a.iframe').fancybox({
+      'type': 'iframe',
+      'width': 600,
+      'height': 600
     });
+  }
+
+  $('button[name="processCarrier"]').click(function (event) {
+    /* Avoid any further action */
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (aeuc_has_virtual_products === true && aeuc_controller.checkVirtualProductRevocation() === false) {
+      var to_display = $('<div/>').html(aeuc_virt_prod_err_str).text();
+      $.fancybox(to_display, {
+        minWidth: 'auto',
+        minHeight: 'auto'
+      });
+      return;
+    }
+    $('<input>', {
+      type: 'hidden',
+      id: 'processCarrier',
+      name: 'processCarrier',
+      value: '1'
+    }).appendTo('#form');
+
+    $("#form").submit();
+  });
 
 });
 
-var AEUC_Controller = function() {
+var AEUC_Controller = function () {
 
-    this.checkVirtualProductRevocation = function() {
-        if ($('#revocation_vp_terms_agreed').prop('checked')) {
-            return true;
-        }
-
-        return false;
+  this.checkVirtualProductRevocation = function () {
+    if ($('#revocation_vp_terms_agreed').prop('checked')) {
+      return true;
     }
+
+    return false;
+  }
 };
