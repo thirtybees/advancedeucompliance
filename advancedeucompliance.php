c106344c37f365f657704bfd311d2d43ded3f618
Petr Hučík <petr@getdatakick.com>
2021-03-02 15:04:40 +0100

    Version 3.1.4

--- advancedeucompliance.php
+++ advancedeucompliance.php
@@ -74,7 +74,7 @@ class Advancedeucompliance extends Module
     ) {
         $this->name = 'advancedeucompliance';
         $this->tab = 'administration';
-        $this->version = '3.1.3';
+        $this->version = '3.1.4';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
