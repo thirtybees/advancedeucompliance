2e4903cb45891b7b1e141e856fb825efde50ae77
therampagerado <office@rampagesport.eu>
2024-05-07 15:53:44 +0300

    Update years and use HTTPS for links in license headers 2 (#22)

--- views/js/email_attachement.js
+++ views/js/email_attachement.js
@@ -16,7 +16,7 @@
  *
  * @author    Thirty Bees <modules@thirtybees.com>
  * @author    PrestaShop SA <contact@prestashop.com>
- * @c0pyright 2017-2024 thirty bees
+ * @copyright 2017-2024 thirty bees
  * @copyright 2007-2016 PrestaShop SA
  * @license   https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  * PrestaShop is an internationally registered trademark & property of PrestaShop SA
